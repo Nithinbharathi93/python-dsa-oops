4373651734a0f66a46dd9ce36131b391e7df81c5
Nithinbharathi93 <nithinthelordest@gmail.com>
2025-09-11 10:01:28 +0530

    sort push

---
+++ sorting/pegionhole.py
@@ -0,0 +1,31 @@
+arr = [5, 5, 3, 4, 1, 7, 4]
+tar = 4
+
+min_value = min(arr)
+max_value = max(arr)
+
+N = max_value - min_value
+
+hole = {i:[] for i in range(N+1)}
+
+for i in arr:
+    hole[i-min_value].append(i)
+
+val_list = []
+
+for i in hole.values():
+    val_list.extend(i)
+
+print(val_list)
+
+left, right = 0, len(val_list)
+while (right >= 0):
+    curr = val_list[left] + val_list[right]
+    if curr == tar:
+        print(val_list[left], val_list[right])
+        exit(0)
+    if curr < tar:
+        left += 1
+    else:
+        right -= 1
+print("Not found")
